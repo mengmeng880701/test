4a8960c40f8a4e0ef5a16808efd77e0420364201
mengchien <pk6507223@gmail.com>
2024-11-27 21:00:08 +0800

    加入專案檔案。

---
+++ Application/Shared/Shared.Model/Authorize/DtoSendSMSVerifyCode.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Model.Authorize
+{
+    public class DtoSendSMSVerifyCode
+    {
+
+        /// <summary>
+        /// 手机号
+        /// </summary>
+        public string Phone { get; set; }
+
+    }
+}
---
+++ ProjectCore/TaskService.Core/InitTaskBackgroundService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using TaskService.Core.QueueTask;
+using TaskService.Core.ScheduleTask;
+
+namespace TaskService.Core
+{
+    public class InitTaskBackgroundService : BackgroundService
+    {
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var taskClasses = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(TaskBase)));
+
+            foreach (Type cls in taskClasses)
+            {
+                ScheduleTaskBuilder.Builder(cls);
+                QueueTaskBuilder.Builder(cls);
+            }
+
+            await Task.CompletedTask;
+        }
+
+    }
+}
